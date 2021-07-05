0f9eb53a956db4e0ae05de7f15754a334ca3b7de
SANTOSH SHRESTHA <shresthasantosh799@gmail.com>
2021-07-05 13:53:46 +0545

    configure CORS

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module cyantosh/go-csv
 go 1.16
 
 require (
+	github.com/gin-contrib/cors v1.3.1 // indirect
 	github.com/gin-gonic/gin v1.7.2 // indirect
 	github.com/gocarina/gocsv v0.0.0-20210516172204-ca9e8a8ddea8 // indirect
 )
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 
 	"cyantosh/go-csv/csv"
@@ -8,6 +9,13 @@ import (
 
 func main() {
 	router := gin.Default()
+
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"PUT", "PATCH", "GET", "POST", "OPTIONS", "DELETE"},
+		AllowHeaders:     []string{"Content-Type", "Access-Control-Allow-Headers", "Authorization", "X-Requested-With"},
+		AllowCredentials: true,
+	}))
 
 	router.GET("/export-csv", csv.ExportInCSV)
 
